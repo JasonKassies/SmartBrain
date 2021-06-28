158d3825e2850699bfc218cceaf703b07ee6725f
Jason k <jason@kassies.ca>
2021-06-28 13:34:57 -0400

    adding background polygons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import './App.css';
-import Navigation from './Components/Navigation/Navigation'
-import Logo from './Components/Logo/Logo'
-import Rank from './Components/Rank/Rank'
-import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import React from 'react';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import Rank from './Components/Rank/Rank';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Particles from 'react-particles-js';
+
+const particlesOpts = {
+	particles: {
+		number: {
+			value: 30,
+			density: {
+				enable: true,
+				value_area: 800
+			}
+		}
+	}
+}
+
 
 function App() {
   return (
     <div className="App">
+		<Particles className='particles' params={particlesOpts} />
     	<Navigation />
 		<Logo />
 		<Rank />
