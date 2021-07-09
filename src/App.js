fd7e0252abeb514d68d8722a5fbe192f202c2a3a
Jason k <jason@kassies.ca>
2021-07-09 13:29:10 -0400

    face box still dont work but sign in/sign out and registration pages work

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navigation from './Components/Navigation/Navigation';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
+import SignIn from './Components/SignIn/SignIn';
+import Register from './Components/Register/Register';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai'
@@ -32,31 +34,33 @@ class App extends Component {
 		this.state = {
 			input : '',
 			imageUrl: '',
-			box: {}
+			stateBox: {},
+			route: 'signin',
+			isSignedIn: false,
 		}
 	}
 
-	calculateFaceLocation = (input) => {
-		const face = input.outputs[0].data.regions[0].region_info.bounding_box;
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
 		const image = document.getElementById('inputimage');
 		const width = Number(image.width);
 		const height = Number(image.height);
-		console.log(input.outputs[0].data.regions[0].data.concepts[0].name);
+		console.log(data.outputs[0].data.regions[0].data.concepts[0].name);
 		return {
-			top_row: face.top_row * height,
-			left_col: face.left_col * width,
-			bottom_row: height - (face.bottom_row * height),
-			right_col: width - (face.right_col * width),
+			leftCol: clarifaiFace.left_col * width,
+      		topRow: clarifaiFace.top_row * height,
+     		rightCol: width - (clarifaiFace.right_col * width),
+     		bottomRow: height - (clarifaiFace.bottom_row * height)
 		}
 	}
 
 	displayFaceBox = (newBox) => {
-		this.setState = ({box: newBox});
+		this.setState = ({stateBox: newBox});
 		console.log(newBox);
 	}
 
 	onInputChange = (event) => {
-		this.setState({input: event.target.value})
+		this.setState({input: event.target.value});
 	}
 
 	onButtonSubmit = () => {
@@ -64,20 +68,36 @@ class App extends Component {
 		app.models.predict(
 			'e466caa0619f444ab97497640cefc4dc', 
 			this.state.input)
-			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-			.catch(err => console.log(err))
+		.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+		.catch(err => console.log(err))
+	}
+
+	onRouteChange = (route) => {
+		if (route === 'signout') {
+			this.setState({isSignedIn: false})
+		} else if (route === 'home') {
+			this.setState({isSignedIn: true})
+		}
+		this.setState({route: route});
 	}
 
 	render() {
 		return (
 			<div className="App">
 				<Particles className='particles' params={particlesOpts} />
-				<Navigation />
-				<Logo />
-				<Rank />
-				<ImageLinkForm onInputChange={this.onInputChange} 
-							onButtonSubmit={this.onButtonSubmit}/>
-				<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+				<Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
+				{ this.state.route === 'home'
+					? <div>
+						<Logo />
+						<Rank />
+						<ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+						<FaceRecognition box={this.state.stateBox} imageUrl={this.state.imageUrl}/>
+					</div>
+					: (this.state.route === 'signin' 
+						? <SignIn onRouteChange={this.onRouteChange}/>
+						: <Register onRouteChange={this.onRouteChange}/>
+					)
+				}
 			</div>
 		);
 	}
